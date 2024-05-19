e6ddc27cef69397a52b3b74ac7331ca486d52394
SesuponeSi <sesuponesi@gmail.com>
2024-05-19 07:50:58 -0600

    PruebaCommit3

---
+++ src/Juan.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ * @author Brandon J. Chacón S.
+ * @Version 19 may 2024
+ */
+
+public class Juan {
+
+}
